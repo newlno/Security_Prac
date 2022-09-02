961cc03bd43582f9e30bf7796de774402ef4cbea
NewL <taemin1104@naver.com>
2022-09-03 02:30:22 +0900

    Initial commit

---
+++ src/test/java/com/example/Security_Prac/SecurityPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Security_Prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
